8a1fb5a33d851e0145efd9bb2ea0dd682f3079b8
AzazelI <92353548+AzazelI@users.noreply.github.com>
2026-03-12 12:49:24 +0400

    app.py

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # LOAD DATABASE
 # -----------------------------
 
-with open("../motorcycle_specs_database.json", "r", encoding="utf-8") as f:
+with open("motorcycle_specs_database.json", "r", encoding="utf-8") as f:
     database = json.load(f)
 
 
@@ -63,3 +63,4 @@ if __name__ == "__main__":
 
 
     app.run(debug=True)
+
